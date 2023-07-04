a13ee6863778423708756eb804f62ac8f2a1d0c5
Yan Bisyukov <yanbisyukov@gmail.com>
2023-07-04 04:23:36 +0300

    Complete loops tasks

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -2,12 +2,26 @@ package com.epam.loops;
 
 public class HalfPyramid {
 
-    public void printHalfPyramid(int cathetusLength) {
-
-    }
-
     public static void main(String[] args) {
         new HalfPyramid().printHalfPyramid(8);
     }
 
+    public void printHalfPyramid(int cathetusLength) {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 1; i <= cathetusLength; i++) {
+            printSpaces(output, cathetusLength - i);
+            printNumbersAscending(output, i);
+            output.append(System.lineSeparator());
+        }
+        System.out.print(output);
+    }
+
+    private void printSpaces(StringBuilder output, int count) {
+        output.append(" ".repeat(Math.max(0, count)));
+    }
+
+    private void printNumbersAscending(StringBuilder output, int count) {
+        output.append("*".repeat(Math.max(0, count)));
+    }
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -2,12 +2,35 @@ package com.epam.loops;
 
 public class Pyramid {
 
-    public void printPyramid(int cathetusLength) {
-
-    }
-
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
 
+    public void printPyramid(int cathetusLength) {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 1; i <= cathetusLength; i++) {
+            printSpaces(output, cathetusLength - i);
+            printNumbersAscending(output, i);
+            printNumbersDescending(output, i);
+            output.append(System.lineSeparator());
+        }
+        System.out.print(output);
+    }
+
+    private void printSpaces(StringBuilder output, int count) {
+        output.append(" ".repeat(Math.max(0, count)));
+    }
+
+    private void printNumbersAscending(StringBuilder output, int count) {
+        for (int i = count; i >= 1; i--) {
+            output.append(i);
+        }
+    }
+
+    private void printNumbersDescending(StringBuilder output, int count) {
+        for (int i = 2; i <= count; i++) {
+            output.append(i);
+        }
+    }
 }
